f82d5b232eadf800d54dcfa3f093186fb0e01874
adbcom <ajdebourbon@xpandata.net>
2026-05-02 14:36:30 +0100

    Add files via upload

---
+++ modules/addition.asm
@@ -0,0 +1,11 @@
+include %reg_names.asm%
+
+;put your two numbers to add in REG6 and REG7
+;result is in REG8
+
+move CONSTANT_0 ALU_INPUT_A
+move REG7 ALU_INPUT_B
+move ALU_OUTPUT REG8
+move REG6 ALU_INPUT_A
+move REG8 ALU_INPUT_B
+move ALU_OUTPUT REG8
